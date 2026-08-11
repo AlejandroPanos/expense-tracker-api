2f36f5b892d7f2186ce8389aaa1f6e71aa56b710
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-11 16:59:22 +0200

    update: User model

--- models.py
+++ models.py
@@ -1,9 +1,25 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Enum
+import enum
+
+
+class UserRole(str, enum.Enum):
+    user = "user"
+    admin = "admin"
 
 
 class User(Base):
     __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False, default=UserRole.user)
+    is_active = Column(Boolean, default=True)
+    phone_number = Column(String, unique=True, nullable=False)
 
 
 class Category(Base):
