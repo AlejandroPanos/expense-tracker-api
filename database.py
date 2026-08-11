54a5191a460b4ce9ef653baef5ac52d51f5faa99
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-11 16:38:32 +0200

    update: Add explanation comments

--- database.py
+++ database.py
@@ -3,15 +3,23 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
+# Load all the env variables from the .env file
 load_dotenv()
 
-
+# Pull the DB URL
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
+# The engine is the core object that manages the connection to the database. This
+# is done by using the `create_engine` function.
+# `check_same_thread`: `False` ensures that requests from different threads don't
+# get blocked. It is a security safeguard, but in development, it is ok to use.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# SessionLocal is a class that gets instantiated and produces a new session object
+# whenever it is called
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Creates a Base class for all of our ORM models
 Base = declarative_base()
