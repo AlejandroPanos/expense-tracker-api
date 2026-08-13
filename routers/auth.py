6345922b9e8ef53ac2568b351fec4ebecc545612
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-13 17:12:57 +0200

    fix: Typo

--- routers/auth.py
+++ routers/auth.py
@@ -20,7 +20,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecaed="auto")
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oath2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
