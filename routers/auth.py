637882807ff4e297c3fd97da5cebcd1f62fe6744
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-14 13:48:49 +0200

    update: Minor fixes and check

--- routers/auth.py
+++ routers/auth.py
@@ -100,10 +100,16 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
 ##############
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, user: CreateUserRequest):
-    if user is None:
+    existing_user = (
+        db.query(Users)
+        .filter((Users.username == user.username) | (Users.email == user.email))
+        .first()
+    )
+
+    if existing_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User details are incomplete",
+            detail="Username or email already registered",
         )
 
     create_user_model = Users(
@@ -112,7 +118,7 @@ async def create_user(db: db_dependency, user: CreateUserRequest):
         first_name=user.first_name,
         last_name=user.last_name,
         hashed_password=bcrypt_context.hash(user.password),
-        role=user.role,
+        role="user",
         is_active=True,
         phone_number=user.phone_number,
     )
@@ -131,7 +137,7 @@ async def login_for_access_token(
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_BAD_REQUEST,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="User unauthorised",
         )
 
@@ -139,4 +145,4 @@ async def login_for_access_token(
         user.username, user.id, user.role, expire_delta=timedelta(minutes=20)
     )
 
-    return {"access_token": token, "token_type": "jwt"}
+    return {"access_token": token, "token_type": "bearer"}
