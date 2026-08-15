33eedde6b2457cec53f20d5e050dfc6f6e95111e
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-15 17:08:23 +0200

    add: update_user_password()

--- routers/users.py
+++ routers/users.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from passlib.context import CryptContext
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 from database import SessionLocal
@@ -10,6 +11,11 @@ from models import Users
 router = APIRouter(prefix="/users", tags=["users"])
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class PasswordUpdateRequest(BaseModel):
+    current_password: str
+    new_password: str = Field(min_length=6)
 
 
 def get_db():
@@ -38,3 +44,32 @@ async def get_user(db: db_dependency, user: user_dependency):
         raise HTTPException(status_code=401, detail="User not authorised")
 
     return user
+
+
+@router.post("/update_password", status_code=status.HTTP_204_NO_CONTENT)
+async def update_user_password(
+    db: db_dependency, user: user_dependency, password_request: PasswordUpdateRequest
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user_model.id != user.get("id"):
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    if not bcrypt_context.verify(
+        password_request.current_password, user_model.hashed_password
+    ):
+        raise HTTPException(status_code=401, detail="Unauthorised user")
+
+    if bcrypt_context.verify(password_request.new_password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail="New password must be different")
+
+    user_model.hashed_password = password_request.new_password
+
+    db.add(user_model)
+    db.commit()
