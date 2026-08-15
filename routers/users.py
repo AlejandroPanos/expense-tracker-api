36074fc9b3a5c64c21d0447ba1fd490c80c8fe3d
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-15 17:00:09 +0200

    add: get_user()

--- routers/users.py
+++ routers/users.py
@@ -1,10 +1,11 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
 from database import SessionLocal
 from auth import get_current_user
+from models import Users
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -24,5 +25,16 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def get_user():
-    pass
+async def get_user(db: db_dependency, user: user_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    user_model = db.query(Users).filter(Users.id == user.id).first()
+
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user_model.id != user.get("id"):
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    return user
