dfdf6dd60f0b072901da8b9aa1d37b3e87ef8f74
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-15 16:47:42 +0200

    update: Remove unused import

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
