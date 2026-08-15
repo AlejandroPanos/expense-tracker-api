ac15a030497eabe804ec93f5f12e23dec658d427
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-15 17:49:04 +0200

    update: Fir import path

--- routers/users.py
+++ routers/users.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 from database import SessionLocal
-from auth import get_current_user
+from .auth import get_current_user
 from models import Users
 
 router = APIRouter(prefix="/users", tags=["users"])
