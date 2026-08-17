d5132376043bd12a397af3a2809dbd85ed69b70f
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-17 14:02:45 +0200

    add: test_return_category()

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -1,7 +1,14 @@
-from routers.users import get_current_user, get_db
+from routers.categories import get_db
+from routers.auth import get_current_user
 from .utils import *
 from models import Category
 from fastapi import status
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
+
+
+def test_return_category(test_category):
+    response = client.get("/categories/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()[0]["name"] == "food"
