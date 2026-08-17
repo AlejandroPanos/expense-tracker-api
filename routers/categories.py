0b77c7d9a476be0f23938b7274d1043dd434211b
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-17 12:13:43 +0200

    add: get_categories()

--- routers/categories.py
+++ routers/categories.py
@@ -1,8 +1,10 @@
 # Imports
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 from database import SessionLocal
+from models import Category, Users
 from .auth import get_current_user
 
 # Router
@@ -20,3 +22,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+# Endpoints
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_categories(db: db_dependency, user: user_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    user_categories = (
+        db.query(Category).filter(Category.owner_id == user.get("id")).all()
+    )
+
+    return user_categories
