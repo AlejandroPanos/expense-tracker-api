86d8dca73c5604ba2e31df1c59d5eeffd276b1a3
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-17 12:22:39 +0200

    add: create_category()

--- routers/categories.py
+++ routers/categories.py
@@ -1,6 +1,7 @@
 # Imports
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 from database import SessionLocal
@@ -24,6 +25,11 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
+# Request classes
+class NewCategoryRequest(BaseModel):
+    name: str
+
+
 # Endpoints
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_categories(db: db_dependency, user: user_dependency):
@@ -35,3 +41,30 @@ async def get_categories(db: db_dependency, user: user_dependency):
     )
 
     return user_categories
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_category(
+    db: db_dependency, user: user_dependency, category_request: NewCategoryRequest
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    existing = (
+        db.query(Category)
+        .filter(
+            Category.owner_id == user.get("id"), Category.name == category_request.name
+        )
+        .first()
+    )
+
+    if existing:
+        raise HTTPException(status_code=400, detail="Category already exists")
+
+    category = Category(name=category_request.name, owner_id=user.get("id"))
+
+    db.add(category)
+    db.commit()
+    db.refresh(category)
+
+    return category
