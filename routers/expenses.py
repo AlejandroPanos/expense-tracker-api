0ef4dfc4d662f3d1f46368b8cee3cc6eec4722b4
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-18 12:32:21 +0200

    add: create_expense() & request object

--- routers/expenses.py
+++ routers/expenses.py
@@ -1,9 +1,13 @@
 # Imports
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 from database import SessionLocal
+from models import Category, Expense
 from routers.auth import get_current_user
+from pydantic import BaseModel, Field, field_validator
+from datetime import date as date_type
 
 # Router
 router = APIRouter(prefix="/expenses", tags=["expenses"])
@@ -21,4 +25,55 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+
+# Request classes
+class CreateExpenseRequest(BaseModel):
+    amount: float = Field(gt=0)
+    description: str = Field(max_length=150)
+    date: date_type
+    category_id: int
+
+    @field_validator("date")
+    @classmethod
+    def date_not_in_future(cls, value):
+        if value > date_type.today():
+            raise ValueError("Expense cannot be in the future.")
+        return value
+
+
 # Endpoints
+@router.post("/", status_code=status.HTTP_200_OK)
+async def create_expense(
+    db: db_dependency, user: user_dependency, expense_request: CreateExpenseRequest
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    category = (
+        db.query(Category)
+        .filter(
+            Category.id == expense_request.category_id,
+            Category.owner_id == user.get("id"),
+        )
+        .first()
+    )
+
+    if category is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found or does not belong to user",
+        )
+
+    expense = Expense(
+        amount=expense_request.amount,
+        description=expense_request.description,
+        date=expense_request.date,
+        owner_id=user.get("id"),
+        category_id=expense_request.category_id,
+    )
+
+    db.add(expense)
+    db.commit()
+    db.refresh(expense)
+
+    return expense
