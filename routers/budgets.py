e7a1b1fca3770858924c91e640a8c53cc4f96e77
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-18 14:25:59 +0200

    add: Budget init & first route

---
+++ routers/budgets.py
@@ -0,0 +1,69 @@
+# Imports
+from typing import Annotated, Optional
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import func
+from sqlalchemy.orm import Session
+from starlette import status
+from database import SessionLocal
+from models import Budget, Category
+from routers.auth import get_current_user
+from pydantic import BaseModel, Field, field_validator
+from datetime import date as date_type
+
+# Router
+router = APIRouter(prefix="/budgets", tags=["budgets"])
+
+
+# Dependencies
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+# Request classes
+class CreateBudgetRequest(BaseModel):
+    category_id: int
+    monthly_limit: float = Field(gt=0)
+
+
+# Endpoints
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_budget(
+    db: db_dependency, user: user_dependency, budget_request: CreateBudgetRequest
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorised")
+
+    category = (
+        db.query(Category)
+        .filter(
+            Category.id == budget_request.category_id,
+            Category.owner_id == user.get("id"),
+        )
+        .first()
+    )
+
+    if category is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found or does not belong to user",
+        )
+
+    budget = Budget(
+        owner_id=user.get("id"),
+        category_id=budget_request.category_id,
+        monthly_limit=budget_request.monthly_limit,
+    )
+
+    db.add(budget)
+    db.commit()
+    db.refresh(budget)
+
+    return budget
