ad286927c922434c11f6b972623330758f2bee87
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-19 09:24:55 +0200

    add: Expenses & budgets routers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database import engine
 import models
-from routers import auth, users, categories
+from routers import auth, users, categories, expenses, budgets
 
 app = FastAPI()
 
@@ -10,3 +10,5 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(categories.router)
+app.include_router(expenses.router)
+app.include_router(budgets.router)
