affc231ae364961af2f63b925a70eec787b618ee
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-21 17:23:22 +0200

    update: Delete unused import

--- tests/test_budgets.py
+++ tests/test_budgets.py
@@ -3,7 +3,6 @@ from routers.auth import get_current_user
 from .utils import *
 from models import Budget
 from fastapi import status
-from datetime import date
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
