c58e498046e4fc05b0149df80fab15168c09bc77
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-21 17:24:52 +0200

    add: Comments

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# Imports
 from fastapi import FastAPI
 from database import engine
 import models
 from routers import auth, users, categories, expenses, budgets
 
+# Create the app
 app = FastAPI()
 
+# Create the engine
 models.Base.metadata.create_all(bind=engine)
 
+# Use all the routers
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(categories.router)
