d16caae89978c32408c9d3af74bd591600783c70
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-08-22 22:03:25 +0200

    add: Hashed password verification

--- tests/test_users.py
+++ tests/test_users.py
@@ -28,6 +28,12 @@ def test_update_password(test_user):
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
 
+    db = TestingSessionLocal()
+    updated_user = db.query(Users).filter(Users.id == test_user.id).first()
+
+    assert bcrypt_context.verify("alextest12", updated_user.hashed_password)
+    assert not bcrypt_context.verify("123456", updated_user.hashed_password)
+
 
 def test_update_phone(test_user):
     response = client.put("/users/phone_number", json={"phone": "+34606606606"})
